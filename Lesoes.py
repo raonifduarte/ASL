92bbef78e4e566bfa8aab6b874c0814b84276351
raonifduarte <80927549+raonifduarte@users.noreply.github.com>
2021-06-16 12:52:54 -0300

    Add files via upload

---
+++ Lesoes.py
@@ -0,0 +1,657 @@
+
+
+import random
+
+a = 'Atlanta Flames'
+b = 'Chicago Pirates'
+c = 'Cincinnati Kings'
+d = 'Dallas Devils'
+e = 'Denver Mountaineers'
+f = 'Houston Wings'
+g = 'Idaho Golden Eagles'
+h = 'Kansas City Reins'
+i = 'Louisville Stallions'
+j = 'Miami Blues'
+k = 'Minesota Stars'
+m = 'New Orleans Vipers'
+n = 'New York Dragons'
+o = 'Philadelphia Sparks'
+p = 'Phoenix Lions'
+q = 'Portland Thorns'
+r = 'Sacramento Miners'
+s = 'San Diego Bolts'
+t = 'Seattle Hornets'
+u = 'Washington Knights'
+
+Atl1 = 'Kyle Chin  '
+Atl2 = 'James Bohanon'
+Atl3 = 'Fraser Colmer'
+Atl4 = 'Kaz Boschen'
+Atl5 = 'Jamie Summers'
+Atl6 = 'Austin Malone'
+Atl7 = 'Ignacio Salas'
+Atl8 = 'Owen Slinker'
+Atl9 = 'Daniel Kuzemka'
+Atl10 = 'Elliot Collier'
+Atl11 = 'Robert Casner'
+Atl12 = 'James White'
+Atl13 = 'Christopher Lema'
+Atl14 = 'Declan McCabe'
+Atl15 = 'Matt Escobar'
+Atl16 = 'Donovan Pollock'
+Atl17 = 'Koby Johnson'
+Atl18 = 'Davey Mason'
+Atl19 = 'Finn Tiedemann'
+Atl20 = 'Aitor Blanco'
+Atl21 = 'Myles Yorke'
+Atl22 = 'Devell Jones'
+Atl23 = 'Nick Bono'
+
+Chi1 = 'Andrew Laryea'
+Chi2 = 'David Martin'
+Chi3 = 'Felix Schaefer'
+Chi4 = 'Justin Saravia'
+Chi5 = 'Ben Minter'
+Chi6 = 'Drew Smith'
+Chi7 = 'Amir Najib'
+Chi8 = 'Rony Murphy'
+Chi9 = 'Shevin Benitez'
+Chi10 = 'Sean Stowe'
+Chi11 = 'Tom Cole'
+Chi12 = 'Jacob David'
+Chi13 = 'Tobenna Thompson'
+Chi14 = 'Sebastian Ferreira'
+Chi15 = 'Gonzalo Escalante'
+Chi16 = 'Zhuvonte Wilson'
+Chi17 = 'Esezi Brooks II'
+Chi18 = 'Tyler Hutchinson'
+Chi19 = 'CC Uche'
+Chi20 = 'Taylor Morrell'
+Chi21 = 'Geoffrey Schaefer'
+Chi22 = 'Yareb Martinez'
+Chi23 = 'Meshack Eshun Addy'
+
+Cin1 = 'Denzel Hasty'
+Cin2 = 'Calum Bishop'
+Cin3 = 'Brian Vail'
+Cin4 = 'Kyle Appiah'
+Cin5 = 'Ricardo Iafrate'
+Cin6 = 'Jaylen Davila-Smith'
+Cin7 = 'Patrick Hellmann'
+Cin8 = 'Jairon Power'
+Cin9 = 'William Pauls'
+Cin10 = 'Max Wilschrey'
+Cin11 = 'Kyle Morrison'
+Cin12 = 'Krishna Nainani'
+Cin13 = 'Danny Elliot'
+Cin14 = 'Michael Speed'
+Cin15 = 'Lucas Imrie'
+Cin16 = 'Keanu Mendoza'
+Cin17 = 'Mason Deeds'
+Cin18 = 'Justin Worley'
+Cin19 = 'Nick Jewell'
+Cin20 = 'AJ Lotsis'
+Cin21 = 'Grant Mafe'
+Cin22 = 'Dan Frisch'
+Cin23 = 'Jeremiah Burke'
+
+Dal1 = 'Pasha Hashemi'
+Dal2 = 'Boaz Myhill'
+Dal3 = 'Alfred Edmonds'
+Dal4 = 'Benjamin Kucera'
+Dal5 = 'Todd Oakley'
+Dal6 = 'Edson Cardona'
+Dal7 = 'Solo George'
+Dal8 = 'Lesley Nchanj'
+Dal9 = 'Oliver Padilla'
+Dal10 = 'Matt Steiurer'
+Dal11 = 'Andres Jimenez'
+Dal12 = 'Tyler Biggs'
+Dal13 = 'Josh Ogbonnaya'
+Dal14 = 'Sean Parker'
+Dal15 = 'Luis Urias'
+Dal16 = 'JB Fischer'
+Dal17 = 'Josh Frouchauer'
+Dal18 = 'Ryan Langkafel'
+Dal19 = 'Siverth Daehlie'
+Dal20 = 'Chris Hodan'
+Dal21 = 'Tony Magalhaes'
+Dal22 = 'Tim McCahery'
+Dal23 = 'Matt Williamson'
+
+Den1 = 'Chris Johnston'
+Den2 = 'Rhys Taylor'
+Den3 = 'Jaime Miralles'
+Den4 = 'Skyler Davis'
+Den5 = 'Brian Wright'
+Den6 = 'Eric Venezia'
+Den7 = 'Arthur Bacquet'
+Den8 = 'Fox Slotemaker'
+Den9 = 'Bernard Yeboah'
+Den10 = 'Brendan Torre'
+Den11 = 'Johncarlo Smith'
+Den12 = 'Phil Nixon'
+Den13 = 'Jerome Marinho'
+Den14 = 'Christopher White'
+Den15 = 'Dylan Nealis'
+Den16 = 'Kyle Zajec'
+Den17 = 'Solomon Rush'
+Den18 = 'Jaime Blalock'
+Den19 = 'Kyle Bellu'
+Den20 = 'Chris Brasile'
+Den21 = 'Kmoosah Khanat'
+Den22 = 'Nicolas Mertzokat'
+Den23 = 'Zack Stavrou'
+
+Hou1 = 'Erick Allez'
+Hou2 = 'R.J. Texada'
+Hou3 = 'Grayland Lynch'
+Hou4 = 'Michael Wetungu'
+Hou5 = 'Brandon Williams'
+Hou6 = 'Mark Bullard'
+Hou7 = 'Taylor Martin'
+Hou8 = 'Dion Van der Aar'
+Hou9 = 'Christian Soldat'
+Hou10 = 'Nico Bermudez'
+Hou11 = 'Ryan Baird'
+Hou12 = 'Yanni Rigos'
+Hou13 = 'Jimmy Keefe'
+Hou14 = 'Diego Aguirre'
+Hou15 = 'Connor Doyle'
+Hou16 = 'Ethan Koehler'
+Hou17 = 'Tanner Anderson'
+Hou18 = 'Hanry Chancy'
+Hou19 = 'Hunter Robertson'
+Hou20 = 'Kiondre Harris'
+Hou21 = 'Alex Nihols'
+Hou22 = 'Dennis Sanner'
+Hou23 = 'Tommy Quirk'
+
+Idh1 = 'Tanner Thompson'
+Idh2 = 'Kalon Brewer'
+Idh3 = 'Chris Zeno'
+Idh4 = 'Terrell Jones'
+Idh5 = 'Trevor Kerns'
+Idh6 = 'Hayden Mbiwa'
+Idh7 = 'Danny Gonzalez'
+Idh8 = 'Aidan Aylward'
+Idh9 = 'Marcel da Silva'
+Idh10 = 'Alex Prusa'
+Idh11 = 'Alan Dawson'
+Idh12 = 'Bobby Edwards'
+Idh13 = 'Thomas Germano'
+Idh14 = 'Bradley Keller'
+Idh15 = 'Riley Strassner'
+Idh16 = 'Brandon Gillingham'
+Idh17 = 'Henrique Joaquim'
+Idh18 = 'Dean Rutherford'
+Idh19 = 'Connor Markulec'
+Idh20 = 'Everett McCord'
+Idh21 = 'Zack Decker'
+Idh22 = 'Derek Biss'
+Idh23 = 'Jack Beer'
+
+Kc1 = 'Jordan Buescher'
+Kc2 = 'Tyler Edwards'
+Kc3 = 'Collin Webb'
+Kc4 = 'Brandt Bronico'
+Kc5 = 'Trestan Hanspard Jr.'
+Kc6 = 'Peter brodeur'
+Kc7 = 'CJ Taylor'
+Kc8 = 'Alexander Liget'
+Kc9 = 'Keegan Brett'
+Kc10 = 'Jason Klinge II'
+Kc11 = 'Jared Kent'
+Kc12 = 'Eric Williams'
+Kc13 = 'John Missimo'
+Kc14 = 'Aidan Megally'
+Kc15 = 'Edson Martinez'
+Kc16 = 'Suniel Veerakone'
+Kc17 = 'Patrick Pray'
+Kc18 = 'James Kramer'
+Kc19 = 'Sean Bowman'
+Kc20 = 'Jordan Potts'
+Kc21 = 'Lucas Morato'
+Kc22 = 'Banks Fisher'
+Kc23 = 'Max Salazar'
+
+Lou1 = 'Alex Ranalli'
+Lou2 = 'Jimmy Hague'
+Lou3 = 'Omar Herbers'
+Lou4 = 'Garrett Bass'
+Lou5 = 'Edu Teixeira'
+Lou6 = 'Malick Mbaye'
+Lou7 = 'Lucas Valciunas'
+Lou8 = 'Ximo Mirallas'
+Lou9 = 'Shane Devine'
+Lou10 = 'Jack Miskel'
+Lou11 = 'Nick Weiners'
+Lou12 = 'Ethan Lochner'
+Lou13 = 'Matt McGuire'
+Lou14 = 'Jaylen Jones'
+Lou15 = 'Zack Knudson'
+Lou16 = 'Andrew Nakamura'
+Lou17 = 'Noah Waxner'
+Lou18 = 'Darrin MacLeod'
+Lou19 = 'Jorge Burke'
+Lou20 = 'Shane Haley'
+Lou21 = 'Cyle Shelton'
+Lou22 = 'Albert Idowu'
+Lou23 = 'Timo Gamble'
+
+Mia1 = 'Kyle Whigham'
+Mia2 = 'Lucas Valilo'
+Mia3 = 'John Black'
+Mia4 = 'Andrew Hoang'
+Mia5 = 'Lucas McBride'
+Mia6 = 'Kolby Copeland'
+Mia7 = 'Mark Sloan'
+Mia8 = 'Kevin Politz'
+Mia9 = "D'Shon DeGannes"
+Mia10 = 'Andrew Bong'
+Mia11 = 'Brian White'
+Mia12 = 'Eduvie Ikoba'
+Mia13 = 'T.J. Butzky'
+Mia14 = 'Oscar Greene'
+Mia15 = 'Rob Landry'
+Mia16 = 'Jake Areman'
+Mia17 = 'Delix De Bona'
+Mia18 = 'Liam Sparrow'
+Mia19 = 'Amass Casner'
+Mia20 = 'Jamal Porter'
+Mia21 = 'Adam Wright'
+Mia22 = 'Ryan Wells'
+Mia23 = 'Amir Oduye'
+
+Min1 = 'Wyatt Omsberg'
+Min2 = 'Ryan Sierakowski'
+Min3 = 'Florian Valot'
+Min4 = 'Cody Sweatte'
+Min5 = 'Abram Baucum'
+Min6 = 'Will Perdue'
+Min7 = 'Erik Hanson'
+Min8 = 'Ryan McLaughlin'
+Min9 = 'Chris Lurie'
+Min10 = 'Dominic Wooldridge'
+Min11 = 'Marius Flateboe'
+Min12 = 'Sonimi Halliday'
+Min13 = 'Noah Roy'
+Min14 = 'Carlos Clark'
+Min15 = 'Johnathan Garrett'
+Min16 = 'Christian Flath'
+Min17 = 'Casey Oliver'
+Min18 = 'John Antunes'
+Min19 = 'Derek Dodson'
+Min20 = 'Brian Wiese'
+Min21 = 'Edward Cole'
+Min22 = 'Oliver Shannon'
+Min23 = 'Zach Samol'
+
+No1 = 'John Schuman'
+No2 = "Daniel O'Keefe"
+No3 = 'Zachary Turk'
+No4 = 'Will Vance'
+No5 = 'Evan Szklennik'
+No6 = 'Marshall Turnley'
+No7 = 'Elliot Otmani'
+No8 = 'Jordan Valentic'
+No9 = 'Ivan Alfaro'
+No10 = 'Nick Mayhugh'
+No11 = 'Jared Odenbeck'
+No12 = 'Wade Mitchell'
+No13 = 'Austin Smythe'
+No14 = 'Daniel Krutzen'
+No15 = "Sean O'Hearn"
+No16 = 'Calvin Howard'
+No17 = 'Cole Manga'
+No18 = 'Achara Badu'
+No19 = 'Andy Martinez'
+No20 = 'Kendall McIntosh'
+No21 = 'Alejandro Flores'
+No22 = 'Alex Hajj'
+No23 = 'Josh Fey'
+
+Ny1 = 'Richard Ballard'
+Ny2 = 'Mertcan Akar'
+Ny3 = 'Omar Herbers'
+Ny4 = 'Billy McConnell'
+Ny5 = 'Jared Brewer'
+Ny6 = 'Sean Caulfield'
+Ny7 = 'Eric Mathers'
+Ny8 = 'Paul Pitter'
+Ny9 = 'Michael Reis'
+Ny10 = 'Tennant McVea'
+Ny11 = 'Matej Dekovic'
+Ny12 = 'Femi Hollinger'
+Ny13 = 'Darius Heard'
+Ny14 = 'Stefan Woort-Menker'
+Ny15 = 'Connor Doyle'
+Ny16 = 'Santo Arena'
+Ny17 = 'Phil Fives'
+Ny18 = 'Matt Foldesy'
+Ny19 = 'Cam Brooks'
+Ny20 = 'Kurt Gerteisen'
+Ny21 = 'Mario Ruiz'
+Ny22 = 'Eric Udelson'
+Ny23 = 'Luke Wildy'
+
+Phi1 = 'Michael Galullo'
+Phi2 = 'Paul Ciszewski'
+Phi3 = 'Andreu Cases-Mundet'
+Phi4 = 'Josh Woods'
+Phi5 = 'Adam Goldfaden'
+Phi6 = 'Garrett Walke'
+Phi7 = 'Roger Rojas-Good'
+Phi8 = 'Rafael Fagundo'
+Phi9 = 'Luke Menne'
+Phi10 = 'Jackson Threet'
+Phi11 = 'Jon Bakero'
+Phi12 = 'Krzrystof Nalborski'
+Phi13 = 'Vince Cicciarelli'
+Phi14 = 'Rio Hope-Gund'
+Phi15 = 'Matt Ledder'
+Phi16 = 'Anes Mrkulic'
+Phi17 = 'Christian Lomelli'
+Phi18 = 'Emir James'
+Phi19 = 'Vincent Taintor'
+Phi20 = 'Cody Jewett'
+Phi21 = 'Thomas Haws'
+Phi22 = 'Michael Nakajima'
+Phi23 = 'Jose Aguinaga'
+
+Pho1 = 'Hans Hones'
+Pho2 = 'Andrew Pannenberg'
+Pho3 = 'Nick Woodruff'
+Pho4 = 'Gavin Terry'
+Pho5 = 'Sebastian Hauret'
+Pho6 = 'Andreas Iosifides'
+Pho7 = 'Eric Mubang'
+Pho8 = 'Zach LoBasso'
+Pho9 = 'Brandon Perdue'
+Pho10 = 'Kyle Thompson'
+Pho11 = 'Jake Miller'
+Pho12 = 'Corin Kwasnik'
+Pho13 = 'Faik Hajderovic'
+Pho14 = 'Jay Franklin'
+Pho15 = 'Evan Konermann'
+Pho16 = 'Colin Stripling'
+Pho17 = 'Stefan Cleveland'
+Pho18 = 'Rob Nardiello'
+Pho19 = 'Chris Lee'
+Pho20 = 'Mueng Sunday'
+Pho21 = 'Brock Anderson'
+Pho22 = 'Paul Clowes'
+Pho23 = 'Timere Berry'
+
+Por1 = 'Jacob Gooden'
+Por2 = 'Tripp Morgan'
+Por3 = 'Jameson Barnes'
+Por4 = 'Kyle Daledovich'
+Por5 = 'Niko Klosterhalfen'
+Por6 = 'Ray Saari'
+Por7 = 'Kevin Politz'
+Por8 = 'Matt Jarasinski'
+Por9 = 'Brandon Welter'
+Por10 = 'Davis Pines'
+Por11 = 'Afonso Piñeda'
+Por12 = 'Mitchell Hamilton'
+Por13 = 'Emmanuel Arteaga'
+Por14 = 'Esteban Hidalgo'
+Por15 = 'Cole Zouzoua'
+Por16 = 'James Matarazzo'
+Por17 = 'Foster McCune'
+Por18 = 'George Akampeke'
+Por19 = 'Chris Autman-Bell'
+Por20 = 'Lonnie Moore'
+Por21 = 'Mike Sapp III'
+Por22 = 'Adam Nicholson'
+Por23 = 'Romario Alashe'
+
+Sac1 = 'Giuseppe Barone'
+Sac2 = 'Jimmy Fiscus'
+Sac3 = 'Stefan Lammana'
+Sac4 = 'Martin Riedler'
+Sac5 = 'Cole Herman'
+Sac6 = 'Jacoby Speaks'
+Sac7 = 'Babacar Diallo'
+Sac8 = 'Philip Stair'
+Sac9 = 'Nico Solarrabieta'
+Sac10 = 'Elijah Galvin'
+Sac11 = 'Ryan Alihodzic'
+Sac12 = 'Cameron Moseley'
+Sac13 = 'Cale Thygesen'
+Sac14 = 'Addison Parrish'
+Sac15 = 'Gabe Mayers'
+Sac16 = 'Daniel Grundei'
+Sac17 = 'Antoine Devers'
+Sac18 = 'Neco Hollingsworth'
+Sac19 = 'Jakob Ervin'
+Sac20 = 'Kamal Williams'
+Sac21 = 'Matt Steinberger'
+Sac22 = 'Jack Graziano'
+Sac23 = 'Austin Riddick'
+
+Sd1 = 'Jack Yaro'
+Sd2 = 'Rollie Rocha'
+Sd3 = 'Keegan King'
+Sd4 = 'Jake McGuire'
+Sd5 = 'James Huffer'
+Sd6 = 'Zach Kovan'
+Sd7 = 'William Skoog'
+Sd8 = 'Kevin Politz'
+Sd9 = 'Tyrone White'
+Sd10 = 'Garmon Bedier'
+Sd11 = 'C.B. Jackson'
+Sd12 = 'JT Marcinkowski'
+Sd13 = 'Brian Morley'
+Sd14 = 'Owen Fingar'
+Sd15 = 'Dylan Autran'
+Sd16 = 'Valdemar Anderson'
+Sd17 = 'Reid Conde'
+Sd18 = 'Munashe Raranje'
+Sd19 = 'Erik Bateman'
+Sd20 = 'Andrew Gee'
+Sd21 = 'Sam Graving'
+Sd22 = 'Keegan Carroll'
+Sd23 = 'Mohamed Jelen'
+
+Sea1 = 'Anthoy Viteri'
+Sea2 = 'Ryan Herbert'
+Sea3 = 'Bryan Seefeld'
+Sea4 = 'Zeke Pitre'
+Sea5 = 'Ben Venhaus'
+Sea6 = 'Sindri Kristinsson'
+Sea7 = 'Hans Kroschwitz'
+Sea8 = 'Dru Jackson'
+Sea9 = 'Luke Waehchter'
+Sea10 = 'Maurice Coyle'
+Sea11 = 'Casey Gleeson'
+Sea12 = 'Christoph Taylor'
+Sea13 = 'Sergio Camargo'
+Sea14 = 'Lucas Bartlett'
+Sea15 = 'David Pastuna'
+Sea16 = 'Chris Miller'
+Sea17 = 'Terell Johson'
+Sea18 = 'Witt Conger'
+Sea19 = 'Javan Ike'
+Sea20 = 'Colin Marz'
+Sea21 = 'Benjamin Harris'
+Sea22 = 'Joseph Panuccio'
+Sea23 = 'Zach Verso'
+
+Was1 = 'Alberto Gorini'
+Was2 = 'Christian Coulson'
+Was3 = 'Noah Sinkoff'
+Was4 = 'Zach Martin'
+Was5 = 'Edison Giron'
+Was6 = 'Brennan Castro'
+Was7 = 'Carlos Delgadillo'
+Was8 = 'Jordan Delashaw'
+Was9 = 'Ross Lanham'
+Was10 = 'Chris Harmon'
+Was11 = 'Tyquan Henderson'
+Was12 = 'David Asbjornsson'
+Was13 = 'Ben Snyder'
+Was14 = 'Connor Doyle'
+Was15 = 'Jacob Graiber'
+Was16 = 'Gideon Rosenthal'
+Was17 = 'Luke Bromley'
+Was18 = 'Jacob Montes'
+Was19 = 'Adam Malekos'
+Was20 = 'Justus Thompson'
+Was21 = 'Alex Peñaranda'
+Was22 = 'Connor Smith'
+Was23 = 'Chris Seager'
+
+times = [a, b, c, d, e, f, g, h, i, j, k, m, n, o, p, q, r, s, t, u]
+
+semanas = [1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+Atlanta = [Atl1, Atl2, Atl3, Atl4, Atl5, Atl6, Atl7, Atl8, Atl9, Atl10, Atl11,
+           Atl12, Atl13, Atl14, Atl15, Atl16, Atl17, Atl18, Atl19, Atl20,
+           Atl21, Atl22, Atl23]
+
+Chicago = [Chi1, Chi2, Chi3, Chi4, Chi5, Chi6, Chi7, Chi8, Chi9, Chi10, Chi11,
+           Chi13, Chi14, Chi15, Chi16, Chi17, Chi18, Chi19, Chi20,
+           Chi21, Chi22, Chi23]
+
+Cincinnati = [Cin1, Cin2, Cin3, Cin4, Cin5, Cin6, Cin7, Cin8, Cin9, Cin10,
+              Cin11, Cin12, Cin13, Cin14, Cin15, Cin16, Cin17, Cin18, Cin19,
+              Cin20, Cin21, Cin22, Cin23]
+
+Dallas = [Dal1, Dal2, Dal3, Dal4, Dal5, Dal6, Dal7, Dal8, Dal9, Dal10, Dal11,
+          Dal12, Dal13, Dal14, Dal15, Dal16, Dal17, Dal18, Dal19, Dal20,
+          Dal21, Dal22, Dal23]
+
+Denver = [Den1, Den2, Den3, Den4, Den5, Den6, Den7, Den8, Den9, Den10, Den11,
+          Den12, Den13, Den14, Den15, Den16, Den17, Den18, Den19, Den20,
+          Den21, Den22, Den23]
+
+Houston = [Hou1, Hou2, Hou3, Hou4, Hou5, Hou6, Hou7, Hou8, Hou9, Hou10,
+           Hou11, Hou12, Hou13, Hou14, Hou15, Hou16, Hou17, Hou18, Hou19,
+           Hou20, Hou21, Hou22, Hou23]
+
+Idaho = [Idh1, Idh2, Idh3, Idh4, Idh5, Idh6, Idh7, Idh8, Idh9, Idh10,
+         Idh11, Idh12, Idh13, Idh14, Idh15, Idh16, Idh17, Idh18, Idh19,
+         Idh20, Idh21, Idh22, Idh23]
+
+Kansas_City = [Kc1, Kc2, Kc3, Kc4, Kc5, Kc6, Kc7, Kc8, Kc9, Kc10,
+               Kc11, Kc12, Kc13, Kc14, Kc15, Kc16, Kc17, Kc18, Kc19,
+               Kc20, Kc21, Kc22, Kc23]
+
+Louisville = [Lou1, Lou2, Lou3, Lou4, Lou5, Lou6, Lou7, Lou8, Lou9,
+              Lou10, Lou11, Lou12, Lou13, Lou14, Lou15, Lou16, Lou17, Lou18,
+              Lou19, Lou20, Lou21, Lou22, Lou23]
+
+Miami = [Mia1, Mia2, Mia3, Mia4, Mia5, Mia6, Mia7, Mia8, Mia9, Mia10,
+         Mia11, Mia12, Mia13, Mia14, Mia15, Mia16, Mia17, Mia18, Mia19,
+         Mia20, Mia21, Mia22, Mia23]
+
+Minesota = [Min1, Min2, Min3, Min4, Min5, Min6, Min7, Min8, Min9, Min10,
+            Min11, Min12, Min13, Min14, Min15, Min16, Min17, Min18, Min19,
+            Min20, Min21, Min22, Min23]
+
+New_Orleans = [No1, No2, No3, No4, No5, No6, No7, No8, No9, No10, No11,
+               No12, No13, No14, No15, No16, No17, No18, No19,
+               No20, No21, No22, No23]
+
+New_York = [Ny1, Ny2, Ny3, Ny4, Ny5, Ny6, Ny7, Ny8, Ny9, Ny10,
+            Ny11, Ny12, Ny13, Ny14, Ny15, Ny16, Ny17, Ny18, Ny19,
+            Ny20, Ny21, Ny22, Ny23]
+
+Philadelphia = [Phi1, Phi2, Phi3, Phi4, Phi5, Phi6, Phi7, Phi8, Phi9,
+                Phi10, Phi11, Phi12, Phi13, Phi14, Phi15, Phi16, Phi17, Phi18,
+                Phi19, Phi20, Phi21, Phi22, Phi23]
+Phoenix = [Pho1, Pho2, Pho3, Pho4, Pho5, Pho6, Pho7, Pho8, Pho9, Pho10,
+           Pho11, Pho12, Pho13, Pho14, Pho15, Pho16, Pho17, Pho18, Pho19,
+           Pho20, Pho21, Pho22, Pho23]
+
+Portland = [Por1, Por2, Por3, Por4, Por5, Por6, Por7, Por8, Por9, Por10,
+            Por11, Por12, Por13, Por14, Por15, Por16, Por17, Por18, Por19,
+            Por20, Por21, Por22, Por23]
+Sacramento = [Sac1, Sac2, Sac3, Sac4, Sac5, Sac6, Sac7, Sac8, Sac9, Sac10,
+              Sac11, Sac12, Sac13, Sac14, Sac15, Sac16, Sac17, Sac18, Sac19,
+              Sac20, Sac21, Sac22, Sac23]
+San_Diego = [Sd1, Sd2, Sd3, Sd4, Sd5, Sd6, Sd7, Sd8, Sd9, Sd10, Sd11, Sd12,
+             Sd13, Sd14, Sd15, Sd16, Sd17, Sd18, Sd19, Sd20, Sd21, Sd22, Sd23]
+
+Seattle = [Sea1, Sea2, Sea3, Sea4, Sea5, Sea6, Sea7, Sea8, Sea9, Sea10, Sea11,
+           Sea12, Sea13, Sea14, Sea15, Sea16, Sea17, Sea18, Sea19, Sea20,
+           Sea21, Sea22, Sea23]
+Washington = [Was1, Was2, Was3, Was4, Was5, Was6, Was7, Was8, Was9, Was10,
+              Was11, Was12, Was13, Was14, Was15, Was16, Was17, Was18, Was19,
+              Was20, Was21, Was22, Was23]
+
+a1 = (random.choice(times))
+
+if a1 == a:
+    print('O', (random.choice(Atlanta)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+elif a1 == b:
+    print('O', (random.choice(Chicago)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+elif a1 == c:
+    print('O', (random.choice(Cincinnati)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+elif a1 == d:
+    print('O', (random.choice(Dallas)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+elif a1 == e:
+    print('O', (random.choice(Denver)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+elif a1 == f:
+    print('O', (random.choice(Houston)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+elif a1 == g:
+    print('O', (random.choice(Idaho)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+elif a1 == h:
+    print('O', (random.choice(Kansas_City)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+elif a1 == i:
+    print('O', (random.choice(Louisville)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == j:
+    print('O', (random.choice(Miami)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == k:
+    print('O', (random.choice(Minesota)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == m:
+    print('O', (random.choice(New_Orleans)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == n:
+    print('O', (random.choice(New_York)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == o:
+    print('O', (random.choice(Philadelphia)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == p:
+    print('O', (random.choice(Phoenix)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == q:
+    print('O', (random.choice(Portland)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == r:
+    print('O', (random.choice(Sacramento)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == s:
+    print('O', (random.choice(San_Diego)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == t:
+    print('O', (random.choice(Seattle)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
+
+elif a1 == u:
+    print('O', (random.choice(Washington)), 'do', (a1), 'estará fora por',
+          (random.choice(semanas)), 'semanas por conta de lesão')
---
+++ ordem_randomica.py
@@ -0,0 +1,10 @@
+import random
+
+lista = ['Atlanta', 'Chicago', 'Cincinnati', 'Dallas',
+         'Denver', 'Houston', 'Idaho', 'Kansas City', 'Los Angeles',
+         'Louisville', 'Miami', 'Minnesota', 'New Orleans', 'New York',
+         'Philadelphia', 'Phoenix', 'Portland', 'Sacramento', 'Seattle',
+         'Washington']
+
+random.shuffle(lista)
+print(lista)
